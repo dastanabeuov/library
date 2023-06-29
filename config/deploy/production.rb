07111d3959e1a694f3134ba0ce162803cd7fbcec
ADK <web.dev.adk@gmail.com>
2023-06-29 18:41:37 +0600

    add fix production deploy file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@ set :branch, 'master'
 
 
 set :ssh_options, {
-  keys: %w[/Users/adk/.ssh/id_rsa],
+  keys: %w[/Users/adk/.ssh/id_rsa.pub],
   forward_agent: true,
   auth_methods: %w[publickey password],
   port: 2222
