5274ec99c9223df4bd74f62005b8218e0840b9ed
ADK <web.dev.adk@gmail.com>
2023-06-30 12:33:42 +0600

    add teskas assetsprecompile yarn

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,8 @@ set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
 
 #after 'deploy:finishing', 'whenever:update_crontab'
 
+after 'deploy:updated', 'deploy:precompile_assets'
+
 after 'deploy:publishing', 'passenger:restart'
 
 after 'deploy:publishing', 'application:reload'
